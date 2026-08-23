27967691553a15aae4f830feb0aebc320af93bd2
Raknaos <raknaos@users.noreply.github.com>
2026-08-23 10:08:35 +0200

    Engine v68: keep whole fish stamps

--- js/studio.js
+++ js/studio.js
@@ -9,13 +9,13 @@ import {
   formatCountdown,
   paidBatchSize,
 } from "./auth.js?v=57";
-import { warmup, fastCut, refineCut } from "../lib/engine.js?v=67";
+import { warmup, fastCut, refineCut } from "../lib/engine.js?v=68";
 import {
   bitmapFromSource,
   imageDataFromBitmap,
   blobFromImageData,
   blobFromImageDataBlurred,
-} from "../lib/cutout.js?v=67";
+} from "../lib/cutout.js?v=68";
 
 paintNav();
 warmup();
