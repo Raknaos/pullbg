3d2bf001b129fe0eb4fb0b5adf4775519e39a74f
Raknaos <raknaos@users.noreply.github.com>
2026-08-23 12:00:41 +0200

    Engine v74: keep whole fox stamps

--- js/studio.js
+++ js/studio.js
@@ -9,13 +9,13 @@ import {
   formatCountdown,
   paidBatchSize,
 } from "./auth.js?v=57";
-import { warmup, fastCut, refineCut } from "../lib/engine.js?v=73";
+import { warmup, fastCut, refineCut } from "../lib/engine.js?v=74";
 import {
   bitmapFromSource,
   imageDataFromBitmap,
   blobFromImageData,
   blobFromImageDataBlurred,
-} from "../lib/cutout.js?v=73";
+} from "../lib/cutout.js?v=74";
 
 paintNav();
 warmup();
