f55a23f63d6a7782e0a70c61e4dfab4e3f617e49
Raknaos <raknaos@users.noreply.github.com>
2026-08-24 00:02:22 +0200

    Engine v95: keep whole crab stamps

--- js/studio.js
+++ js/studio.js
@@ -9,13 +9,13 @@ import {
   formatCountdown,
   paidBatchSize,
 } from "./auth.js?v=57";
-import { warmup, fastCut, refineCut } from "../lib/engine.js?v=94";
+import { warmup, fastCut, refineCut } from "../lib/engine.js?v=95";
 import {
   bitmapFromSource,
   imageDataFromBitmap,
   blobFromImageData,
   blobFromImageDataBlurred,
-} from "../lib/cutout.js?v=94";
+} from "../lib/cutout.js?v=95";
 
 paintNav();
 warmup();
