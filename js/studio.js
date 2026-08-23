89ecde43c2c2e72ea3759406e476550eb810d350
Raknaos <raknaos@users.noreply.github.com>
2026-08-23 19:44:46 +0200

    Engine v81: keep whole rooster stamps

--- js/studio.js
+++ js/studio.js
@@ -9,13 +9,13 @@ import {
   formatCountdown,
   paidBatchSize,
 } from "./auth.js?v=57";
-import { warmup, fastCut, refineCut } from "../lib/engine.js?v=80";
+import { warmup, fastCut, refineCut } from "../lib/engine.js?v=81";
 import {
   bitmapFromSource,
   imageDataFromBitmap,
   blobFromImageData,
   blobFromImageDataBlurred,
-} from "../lib/cutout.js?v=80";
+} from "../lib/cutout.js?v=81";
 
 paintNav();
 warmup();
