5a73e499d82413ff44d137b9dcdafa124ede85a9
Raknaos <raknaos@users.noreply.github.com>
2026-08-24 18:35:01 +0200

    Engine v104: keep whole lizard stamps
    Recognize perforated lizard silhouettes (head, tail, two legs) and keep the whole piece while punching holes. Products on white/black stay off the stamp path.

--- js/studio.js
+++ js/studio.js
@@ -9,13 +9,13 @@ import {
   formatCountdown,
   paidBatchSize,
 } from "./auth.js?v=57";
-import { warmup, fastCut, refineCut } from "../lib/engine.js?v=103";
+import { warmup, fastCut, refineCut } from "../lib/engine.js?v=104";
 import {
   bitmapFromSource,
   imageDataFromBitmap,
   blobFromImageData,
   blobFromImageDataBlurred,
-} from "../lib/cutout.js?v=103";
+} from "../lib/cutout.js?v=104";
 
 paintNav();
 warmup();
